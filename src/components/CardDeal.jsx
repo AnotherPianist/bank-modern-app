3d9cd795219de0f377d66788e0722fc843c2e030
ThePianist <cafarimo@outlook.com>
2023-02-23 18:19:51 -0300

    Add CardDeal section

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,3 +1,24 @@
+import styles, { layout } from "../style";
+import Button from "./Button";
+import { card } from "../assets";
+
 export default function CardDeal() {
-  return <div></div>;
+  return (
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Find a better card deal <br className="hidden sm:block" /> in few easy
+          steps.
+        </h2>
+        <p className={`${styles.paragraph} mt-5 max-w-[470px]`}>
+          Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis
+          aliquet eget mauris tortor. Aliquet ultrices ac, ametau.
+        </p>
+        <Button styles="mt-10" />
+      </div>
+      <div className={layout.sectionImg}>
+        <img src={card} alt="card" className="h-full w-full" />
+      </div>
+    </section>
+  );
 }
