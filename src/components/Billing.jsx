e6069fdcbba526eb748c49bdaaf8d4dd0e798cf4
ThePianist <cafarimo@outlook.com>
2023-02-23 18:07:07 -0300

    Add Billing section

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,3 +1,37 @@
+import { apple, bill, google } from "../assets";
+import styles, { layout } from "../style";
+
 export default function Billing() {
-  return <div></div>;
+  return (
+    <section id="billing" className={layout.sectionImgReverse}>
+      <div className={layout.sectionImgReverse}>
+        <img src={bill} alt="billing" className="relative z-20 h-full w-full" />
+        <div className="white__gradient absolute -left-1/2 top-0 z-10 h-1/2 w-1/2 rounded-full" />
+        <div className="pink__gradient absolute -left-1/2 bottom-0 z-0 h-1/2 w-1/2 rounded-full" />
+      </div>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Easily control your <br className="hidden sm:block" /> billing &
+          invoicing.
+        </h2>
+        <p className={`${styles.paragraph} mt-5 max-w-[470px]`}>
+          Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio
+          aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea
+          placerat.
+        </p>
+        <div className="mt-6 flex flex-row flex-wrap sm:mt-10">
+          <img
+            src={apple}
+            alt="apple_store"
+            className="mr-5 h-[42px] w-32 cursor-pointer object-contain"
+          />
+          <img
+            src={google}
+            alt="google_play"
+            className="h-[42px] w-32 cursor-pointer object-contain"
+          />
+        </div>
+      </div>
+    </section>
+  );
 }
