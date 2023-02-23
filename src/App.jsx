eca32ef200ac408b5b50b968e3308cb536cb1855
ThePianist <cafarimo@outlook.com>
2023-02-23 04:03:57 -0300

    Setup components structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
+import {
+  Billing,
+  CardDeal,
+  Clients,
+  CTA,
+  Footer,
+  Hero,
+  Navbar,
+  Stats,
+  Testimonials
+} from "./components";
+import styles from "./style";
+
 export default function App() {
   return (
     <div className="bg-primary w-full overflo-hidden">
-      <h1>Hello world</h1>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
+        </div>
+      </div>
     </div>
   );
 }
